99655cf7ac89338a8b11540931c9c0e8d6db7f8f
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2024-12-19 10:18:04 -0500

    mejora en modelo actividad

--- modules/actividades/validators/actividad.js
+++ modules/actividades/validators/actividad.js
@@ -9,7 +9,7 @@ const validateCreateActividad = [
         .exists().withMessage('La descripción es obligatoria')
         .notEmpty().withMessage('La descripción no puede estar vacía')
         .isLength({ max: 150 }).withMessage('La descripción no puede superar los 150 caracteres'),
-    body('descripcion')
+    body('controlPorLote')
         .exists().withMessage('Control por lote'),
     body('usuario')
         .optional()
