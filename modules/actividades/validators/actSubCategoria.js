02254660582c72b07443fb3d86568b0adc606dc5
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2024-12-26 09:56:58 -0500

    mejora en subcategoria

--- modules/actividades/validators/actSubCategoria.js
+++ modules/actividades/validators/actSubCategoria.js
@@ -1,6 +1,7 @@
 import { body, check, validationResult } from "express-validator";
 
 const validateCreateActSubCategoria = [
+    body('categoriaId').exists().notEmpty(),
     body('nombre').exists().notEmpty().isLength({min: 5, max: 30}),
     body('descripcion').exists().notEmpty().isLength({min: 0, max: 150}),
     body('usuario').optional().isString().withMessage('El usuario debe ser una cadena de texto'),
