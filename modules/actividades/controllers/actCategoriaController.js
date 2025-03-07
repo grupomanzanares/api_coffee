a5a5b4bba198283e526e2a2f5afc45ffedf3de96
ingsandravillamizar <104092220+ingsandravillamizar@users.noreply.github.com>
2025-03-07 07:58:23 -0500

    mejora en consulta de categorias

--- modules/actividades/controllers/actCategoriaController.js
+++ modules/actividades/controllers/actCategoriaController.js
@@ -11,7 +11,7 @@ const getActCategorias = async (req, res) =>{
             include: [
                 {
                     model: Sucursal,
-                    attributes: ["name"]
+                    attributes: ["nombre"]
                 } 
             ]
         });
